4caa2f1b1270e1faa85b4ccee1283e53efcd9423
JesusLibrado <jes.librado@gmail.com>
2018-08-29 15:19:16 -0500

    Update getCurrencies.spec.ts

--- src/app/currencies/getCurrencies.spec.ts
+++ src/app/currencies/getCurrencies.spec.ts
@@ -1,16 +1,9 @@
-i m p o r t { g e t C u r r e n c i e s } from
-describe ( ’ currencies ’ ,
-i t ( ’ should return
-const r e s u l t =
-expect ( r e s u l t ) .
-expect ( r e s u l t ) .
-expect ( r e s u l t ) .
+import {getCurriencies} from './getCurrencies';
+describe ('currencies', ()=>{
+  it('should return the supported currencies', ()=>{
+    const result = getCurrencies();
+    expect(result).toContain('USD');
+    expect(result).toContain('AUD');
+    expect(result).toContain('EUR');
+  })
 })
-})
-’./ getCurrencies ’ ;
-( ) = > { // c u r r e n c i e s s u i t e
-t h e s u p p o r t e d c u r r e n c i e s ’ , ( ) = > {
-getCurrencies ();
-t o C o n t a i n ( ’ USD ’ ) ;
-t o C o n t a i n ( ’AUD ’ ) ;
-t o C o n t a i n ( ’ EUR ’ ) ;
